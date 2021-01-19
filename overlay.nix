7b2f12e47b6b893b29c224573aa4ca77544e2850
John Lotoski <john.lotoski@iohk.io>
2021-01-19 11:49:45 -0500

    Re-add the encrypted-links plugin back to pure builds

--- overlay.nix
+++ overlay.nix
@@ -8,16 +8,10 @@ let
   mkNixops_2 = patches: pkgs.callPackage ./pkgs/nixops_2-unstable.nix {
     inherit self system pkgs patches;
     # Remove libvirtd plugin from Darwin until the compile error is fixed
-    # The plugin "encrypted-links" is also removed due to a bug with the fix not yet merged:
-    #   https://github.com/nix-community/nixops-encrypted-links/pull/1
-    # and also a compatibility issue with at least packet plugin and possibly others.
-    #   https://github.com/nix-community/nixops-encrypted-links/issues/2
     validPlugins = if pkgs.stdenv.isLinux then
-      [ "aws" "gcp" "packet" "packet" "virtd" "vbox" ]
-      # [ "aws" "encrypted-links" "gcp" "packet" "packet" "virtd" "vbox" ]
+      [ "aws" "encrypted-links" "gcp" "packet" "packet" "virtd" "vbox" ]
     else
-      [ "aws" "gcp" "packet" "packet" "vbox" ];
-      # [ "aws" "encrypted-links" "gcp" "packet" "packet" "vbox" ];
+      [ "aws" "encrypted-links" "gcp" "packet" "packet" "vbox" ];
   };
 in final: prev: {
   nixops_1_6_1-preplugin = self.inputs.nixpkgs-pin.legacyPackages.${system}.nixops_1_6_1;
